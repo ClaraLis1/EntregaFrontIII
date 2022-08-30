56d9a8c7e8f905f8b6a79f2b17fc1ac2f916073b
ClaraLis1 <lisleclara1@gmail.com>
2022-08-30 17:10:49 -0300

    Finalizado

---
+++ src/components/Item.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+// El componente Item no tiene componentes hijos.
+// ESTADO: Item debe tener un número para almacenar la cantidad de stock, la misma se la defina el padre a la hora de crearlo.
+// MÉTODOS: Item debe manejar el click de su boton para restar la cantidad en stock de sí mismo y a su vez poder aumentar el estado de su "abuelo" App.
+// PROPS: Item recibe todos los campos que muestra en pantalla: nombre, descripcion, stock y el métodos heredados para su uso.
+// Maqueta de Item:
+//    h3
+//    p
+//    h5 > span    (este span debe mostrar la cantidad si es mayor a 0 "agotado" si llega a 0)
+//    button       (este boton debe permitir comprar, pero si la cantidad es menor a 0 debe estar deshabilitado y decir "Sin stock")
+
+export default function Item(props) { 
+    
+  const[productos, setProductos] = useState(props.stock)
+  const agotado = "agotado"
+
+  
+  function actualizarStock(){
+    if ((productos - 1) >=1){
+      setProductos(productos-1)
+      props.comprar()
+    }else if((productos - 1)===0){
+      setProductos(agotado)
+      props.comprar()  
+    }
+  }  
+
+  return (    
+    <div className="producto">
+      <h3>{props.nombre}</h3>
+      <p>{props.descripcion}</p>
+      <h5>En stock: 
+        <span className={productos===agotado? "" :"inicial"}>{productos}</span>              
+      </h5> 
+      <button  onClick={actualizarStock} disabled= {productos===agotado ? true :  false} > {productos===agotado? "sin stock": "comprar"} </button>
+    </div>    
+  )   
+}
+
